950b017f96af26086601126a6e08b1bba4baf796
lucapug <lucapug@gmail.com>
2024-03-30 18:12:41 +0100

    gcs to bq SQL block with imposed schema

---
+++ mage_dbt_gcp/transformers/gcs_to_bq.sql
@@ -0,0 +1,23 @@
+-- Docs: https://docs.mage.ai/guides/sql-blocks
+-- Creating external table referring to gcs path
+CREATE OR REPLACE EXTERNAL TABLE dtc-de-zoomcamp-2024.nyc_bikes.external_2019_tripdata (
+tripduration INT64,
+starttime TIMESTAMP,
+stoptime TIMESTAMP,
+start_station_id INT64,
+start_station_name STRING,
+start_station_latitude FLOAT64,
+start_station_longitude FLOAT64,
+end_station_id INT64,
+end_station_name STRING,
+end_station_latitude FLOAT64,
+end_station_longitude FLOAT64,
+bike_id INT64,
+usertype STRING,
+birth_year INT64,
+gender INT64
+) 
+OPTIONS (
+  format = 'PARQUET',
+  uris = ['gs://nyc_bike_lucapug/2019_bike_data/*.parquet']
+);
