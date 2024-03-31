b22fa026a91c958d1777dd986a337bae469cdd5c
lucapug <lucapug@gmail.com>
2024-03-31 18:56:09 +0200

    from year hard coded to runtime variable

--- mage_dbt_gcp/transformers/gcs_to_bq.sql
+++ mage_dbt_gcp/transformers/gcs_to_bq.sql
@@ -1,6 +1,6 @@
 -- Docs: https://docs.mage.ai/guides/sql-blocks
 -- Creating external table referring to gcs path
-CREATE OR REPLACE EXTERNAL TABLE dtc-de-zoomcamp-2024.nyc_bikes.external_2019_tripdata (
+CREATE OR REPLACE EXTERNAL TABLE dtc-de-zoomcamp-2024.nyc_bikes.external_{{year}}_tripdata (
 tripduration INT64,
 starttime TIMESTAMP,
 stoptime TIMESTAMP,
@@ -19,5 +19,5 @@ gender INT64
 ) 
 OPTIONS (
   format = 'PARQUET',
-  uris = ['gs://nyc_bike_lucapug/2019_bike_data/*.parquet']
+  uris = ['gs://nyc_bike_lucapug/{{year}}_bike_data/*.parquet']
 );
