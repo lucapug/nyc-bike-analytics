c25da8771817138c1989bab7666cd660d073ef80
lucapug <lucapug@gmail.com>
2024-04-13 17:58:52 +0200

    added to facts table trip_duration and start_lat start_lng

--- mage_dbt_gcp/dbt/dbt_ops/models/core/facts_JC_citibike_trips.sql
+++ mage_dbt_gcp/dbt/dbt_ops/models/core/facts_JC_citibike_trips.sql
@@ -1,8 +1,6 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
+{{ config(
+    materialized='table'
+)}}
 
 with tripdata_history as (
     select *
@@ -11,7 +9,10 @@ with tripdata_history as (
 select rideable_type,
 started_at,
 ended_at,
+TIMESTAMP_DIFF(ended_at, started_at, MINUTE) as duration_in_mins,
 start_station_name,
+start_lat,
+start_lng,
 end_station_name,
 member_casual
 from tripdata_history
